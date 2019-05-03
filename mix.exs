81aef5a5ec93f458734f7cd907f10d90a249f8e5
corka149 <corka149@mailbox.org>
2019-05-03 20:16:19 +0200

    Added poison to dependencies

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Jarvis.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:ueberauth, "~> 0.6"},
-      {:ueberauth_auth0, "~> 0.3"}
+      {:ueberauth_auth0, "~> 0.3"},
+      {:poison, "~> 4.0"}
     ]
   end
 
