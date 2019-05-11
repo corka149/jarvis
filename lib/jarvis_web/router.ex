2c2e4e13d5f55354f437e97ff6b8d58785d8794f
corka149 <corka149@mailbox.org>
2019-05-11 15:45:21 +0200

    Added SetLocale plug to router

--- lib/jarvis_web/router.ex
+++ lib/jarvis_web/router.ex
@@ -8,6 +8,7 @@ defmodule JarvisWeb.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug JarvisWeb.Plugs.SetUser
+    plug JarvisWeb.Plugs.SetLocale, default: "en"
   end
 
   pipeline :api do
