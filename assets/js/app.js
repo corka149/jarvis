5398c857345ae4e8b80751e10ea67b0599256a6e
corka149 <corka149@mailbox.org>
2019-06-02 12:05:59 +0200

    Added printing of shopping lists

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,8 @@ import css from "../css/app.css"
 //
 import "phoenix_html"
 import LiveSocket from "phoenix_live_view"
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 // Import local files
 //
@@ -19,3 +21,25 @@ import LiveSocket from "phoenix_live_view"
 
 let liveSocket = new LiveSocket("/live")
 liveSocket.connect()
+
+function generatePdf() {
+    let doc = new jsPDF();
+    doc.autoTable({ 
+        html: "#open-shopping-list",
+        didDrawCell: data => {
+            console.info(data)
+         },
+         columns: [{header: 'Name', dataKey: 0}, {header: 'Amound', dataKey: 1}]
+    });
+    let date = new Date();
+    let dateStr = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}_${date.getHours()}-${date.getMinutes()}`;
+    doc.save(dateStr + "_shopping-list.pdf");
+}
+
+function main() {
+    if (!!document.getElementById("print-shopping-list")) {
+        document.getElementById("print-shopping-list").onclick = generatePdf;
+    }
+}
+
+main()
