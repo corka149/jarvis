39af17726d64c00c97fca635d384760783069c11
corka149 <corka149@mailbox.org>
2019-06-03 20:58:04 +0200

    Fixed some shopping list controller tests

--- test/jarvis_web/controllers/shopping_list_controller_test.exs
+++ test/jarvis_web/controllers/shopping_list_controller_test.exs
@@ -14,7 +14,7 @@ defmodule JarvisWeb.ShoppingListControllerTest do
 
   def fixture(:shopping_list) do
     {_, user} = Jarvis.Accounts.create_user(@valid_attrs_user)
-    group = Jarvis.Accounts.create_user_group(@valid_attrs_group, user)
+    {_, group} = Jarvis.Accounts.create_user_group(@valid_attrs_group, user)
 
     {:ok, shopping_list} = ShoppingLists.create_shopping_list(@create_attrs, group)
     shopping_list
@@ -24,21 +24,25 @@ defmodule JarvisWeb.ShoppingListControllerTest do
     test "lists all shoppinglists", %{conn: conn} do
       {_, user} = Jarvis.Accounts.create_user(@valid_attrs_user)
       conn = init_test_session(conn, user_id: user.id)
-      conn =  get(conn, Routes.shopping_list_path(conn, :index))
+              |> get(Routes.shopping_list_path(conn, :index))
       assert html_response(conn, 200) =~ "Listing Shoppinglists"
     end
   end
 
   describe "new shopping_list" do
     test "renders form", %{conn: conn} do
-      conn = get(conn, Routes.shopping_list_path(conn, :new))
+      {_, user} = Jarvis.Accounts.create_user(@valid_attrs_user)
+      conn = init_test_session(conn, user_id: user.id)
+              |> get(Routes.shopping_list_path(conn, :new))
       assert html_response(conn, 200) =~ "New Shopping list"
     end
   end
 
   describe "create shopping_list" do
     test "redirects to show when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.shopping_list_path(conn, :create), shopping_list: @create_attrs)
+      {_, user} = Jarvis.Accounts.create_user(@valid_attrs_user)
+      conn = init_test_session(conn, user_id: user.id)
+              |> post(Routes.shopping_list_path(conn, :create), shopping_list: @create_attrs)
 
       assert %{id: id} = redirected_params(conn)
       assert redirected_to(conn) == Routes.shopping_list_path(conn, :show, id)
@@ -48,9 +52,9 @@ defmodule JarvisWeb.ShoppingListControllerTest do
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      user = Jarvis.Accounts.create_user(@valid_attrs_user)
-      assign(conn, :user, user)
-      conn = post(conn, Routes.shopping_list_path(conn, :create), shopping_list: @invalid_attrs)
+      {_, user} = Jarvis.Accounts.create_user(@valid_attrs_user)
+      conn = init_test_session(conn, user_id: user.id)
+              |> post(Routes.shopping_list_path(conn, :create), shopping_list: @invalid_attrs)
       assert html_response(conn, 200) =~ "New Shopping list"
     end
   end
@@ -59,7 +63,10 @@ defmodule JarvisWeb.ShoppingListControllerTest do
     setup [:create_shopping_list]
 
     test "renders form for editing chosen shopping_list", %{conn: conn, shopping_list: shopping_list} do
-      conn = get(conn, Routes.shopping_list_path(conn, :edit, shopping_list))
+      group = Jarvis.ShoppingLists.get_shopping_list!(shopping_list.id).usergroup
+      user = Jarvis.Accounts.get_user_group!(group.id).user
+      conn = init_test_session(conn, user_id: user.id)
+              |> get(Routes.shopping_list_path(conn, :edit, shopping_list))
       assert html_response(conn, 200) =~ "Edit Shopping list"
     end
   end
