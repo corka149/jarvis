438367fede4eab1450ee6606f354fb7d43ffb39b
corka149 <corka149@mailbox.org>
2019-06-05 21:00:47 +0200

    Fixed shopping_lists_tests

--- test/jarvis/shopping_lists_test.exs
+++ test/jarvis/shopping_lists_test.exs
@@ -10,33 +10,45 @@ defmodule Jarvis.ShoppingListsTest do
     @update_attrs %{done: false, planned_for: ~D[2011-05-18]}
     @invalid_attrs %{done: nil, planned_for: nil}
 
+    @valid_attrs_group %{name: "some name"}
+    @valid_attrs_user %{email: "some email", name: "some name", provider: "some provider", token: "some token"}
+
     def shopping_list_fixture(attrs \\ %{}) do
+      {_, user} = Jarvis.Accounts.create_user(@valid_attrs_user)
+      {_, group} = Jarvis.Accounts.create_user_group(@valid_attrs_group, user)
+
       {:ok, shopping_list} =
         attrs
         |> Enum.into(@valid_attrs)
-        |> ShoppingLists.create_shopping_list()
+        |> ShoppingLists.create_shopping_list(group)
 
       shopping_list
     end
 
     test "list_shoppinglists/0 returns all shoppinglists" do
       shopping_list = shopping_list_fixture()
+                      |> Jarvis.Repo.preload(:usergroup)
       assert ShoppingLists.list_shoppinglists() == [shopping_list]
     end
 
     test "get_shopping_list!/1 returns the shopping_list with given id" do
       shopping_list = shopping_list_fixture()
+                      |> Jarvis.Repo.preload(:usergroup)
       assert ShoppingLists.get_shopping_list!(shopping_list.id) == shopping_list
     end
 
     test "create_shopping_list/1 with valid data creates a shopping_list" do
-      assert {:ok, %ShoppingList{} = shopping_list} = ShoppingLists.create_shopping_list(@valid_attrs)
+      {_, user} = Jarvis.Accounts.create_user(@valid_attrs_user)
+      {_, group} = Jarvis.Accounts.create_user_group(@valid_attrs_group, user)
+      assert {:ok, %ShoppingList{} = shopping_list} = ShoppingLists.create_shopping_list(@valid_attrs, group)
       assert shopping_list.done == true
       assert shopping_list.planned_for == ~D[2010-04-17]
     end
 
     test "create_shopping_list/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = ShoppingLists.create_shopping_list(@invalid_attrs)
+      {_, user} = Jarvis.Accounts.create_user(@valid_attrs_user)
+      {_, group} = Jarvis.Accounts.create_user_group(@valid_attrs_group, user)
+      assert {:error, %Ecto.Changeset{}} = ShoppingLists.create_shopping_list(@invalid_attrs, group)
     end
 
     test "update_shopping_list/2 with valid data updates the shopping_list" do
@@ -48,6 +60,7 @@ defmodule Jarvis.ShoppingListsTest do
 
     test "update_shopping_list/2 with invalid data returns error changeset" do
       shopping_list = shopping_list_fixture()
+                      |> Jarvis.Repo.preload(:usergroup)
       assert {:error, %Ecto.Changeset{}} = ShoppingLists.update_shopping_list(shopping_list, @invalid_attrs)
       assert shopping_list == ShoppingLists.get_shopping_list!(shopping_list.id)
     end
