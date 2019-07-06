3c78302e5237cac99ae2075fda852c9a5dcf2352
corka149 <corka149@mailbox.org>
2019-07-06 17:14:28 +0200

    Added "measurement_with_device.json" to MeasurementView

--- lib/jarvis_web/views/measurement_view.ex
+++ lib/jarvis_web/views/measurement_view.ex
@@ -1,18 +1,34 @@
 defmodule JarvisWeb.MeasurementView do
   use JarvisWeb, :view
   alias JarvisWeb.MeasurementView
+  alias JarvisWeb.DeviceView
 
   def render("index.json", %{measurements: measurements}) do
-    %{data: render_many(measurements, MeasurementView, "measurement.json")}
+    %{
+      data: render_many(measurements, MeasurementView, "measurement.json")
+    }
   end
 
   def render("show.json", %{measurement: measurement}) do
-    %{data: render_one(measurement, MeasurementView, "measurement.json")}
+    %{
+      data: render_one(measurement, MeasurementView,"measurement_with_device.json")
+    }
   end
 
   def render("measurement.json", %{measurement: measurement}) do
-    %{id: measurement.id,
+    %{
+      id: measurement.id,
       description: measurement.description,
-      value: measurement.value}
+      value: measurement.value
+    }
+  end
+
+  def render("measurement_with_device.json", %{measurement: measurement}) do
+    %{
+      id: measurement.id,
+      description: measurement.description,
+      value: measurement.value,
+      device: render_one(measurement.device, DeviceView, "device.json")
+    }
   end
 end
