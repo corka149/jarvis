ce5ced997e803bdde8ab7135b50438f22597072f
corka149 <corka149@mailbox.org>
2019-08-03 09:57:04 +0200

    Added chart.js and init method

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,4 +1,6 @@
 import {Socket} from "phoenix"
+import "chart.js"
+
 let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
@@ -6,7 +8,10 @@ socket.connect()
 window.createSocket = () => {
   let channel = socket.channel("measurement", {})
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("ok", resp => { 
+      console.log("Joined successfully", resp) 
+      initiateCharts(resp)
+    })
     .receive("error", resp => { console.log("Unable to join", resp) })
 
   channel.on("measurement:new", (response) => {
@@ -14,3 +19,47 @@ window.createSocket = () => {
   })
 }
 
+function initiateCharts(initialValues) {
+  let ctx = document.getElementById('myChart');
+  let myChart = new Chart(ctx, {
+      type: 'line',
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      data: {
+        labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+        datasets: [{ 
+            data: [86,114,106,106,107,111,133,221,783,2478],
+            label: "Africa",
+            borderColor: "#3e95cd",
+            fill: false
+          }, { 
+            data: [282,350,411,502,635,809,947,1402,3700,5267],
+            label: "Asia",
+            borderColor: "#8e5ea2",
+            fill: false
+          }, { 
+            data: [168,170,178,190,203,276,408,547,675,734],
+            label: "Europe",
+            borderColor: "#3cba9f",
+            fill: false
+          }, { 
+            data: [40,20,10,16,24,38,74,167,508,784],
+            label: "Latin America",
+            borderColor: "#e8c3b9",
+            fill: false
+          }, { 
+            data: [6,3,2,2,7,26,82,172,312,433],
+            label: "North America",
+            borderColor: "#c45850",
+            fill: false
+          }
+        ]
+      },
+      options: {
+        elements: {
+            line: {
+                tension: 0 // disables bezier curves
+            }
+        }
+    }
+  });
+}
