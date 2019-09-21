1e4637d0feb402ba0e8151fc14f48d6f501227d2
corka149 <corka149@mailbox.org>
2019-09-21 10:43:02 +0200

    Added mix release stuff

---
+++ config/releases.exs
@@ -0,0 +1,33 @@
+import Config
+
+config :jarvis, JarvisWeb.Endpoint,
+  http: [:inet6, port: System.fetch_env!("PORT")],
+  url: [host: System.fetch_env!("HOST"), port: System.fetch_env!("PORT")], # This is critical for ensuring web-sockets properly authorize.
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: ".",
+  version: Application.spec(:jarvis, :vsn)
+
+config :logger, level: :info
+
+config :phoenix, :serve_endpoints, true
+
+config :jarvis, JarvisWeb.Endpoint,
+  secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
+
+config :jarvis, Jarvis.Repo,
+  username: System.fetch_env!("DB_USERNAME"),
+  password: System.fetch_env!("DB_PASSWORD"),
+  database: System.fetch_env!("DB_NAME"),
+  hostname: System.fetch_env!("DB_HOST"),
+  pool_size: 15
+
+config :jarvis, Jarvis.ShoppingLists.Vision,
+  host: System.fetch_env!("VISION_HOST"),
+  username: System.fetch_env!("VISION_USERNAME"),
+  password: System.fetch_env!("VISION_PASSWORD")
+
+config :ueberauth, Ueberauth.Strategy.Auth0.OAuth,
+  domain: System.fetch_env!("AUTH0_DOMAIN"),
+  client_id: System.fetch_env!("AUTH0_CLIENT_ID"),
+  client_secret: System.fetch_env!("AUTH0_CLIENT_SECRET")
