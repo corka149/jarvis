550bfc8b8a87ef871e7abad548b4551ffe346d75
corka149 <corka149@mailbox.org>
2019-09-21 10:40:31 +0200

    Create mix task for build docker image

---
+++ lib/mix/tasks/docker.ex
@@ -0,0 +1,10 @@
+defmodule Mix.Tasks.Docker do
+  use Mix.Task
+
+  @shortdoc "Creates a docker image with tags"
+  def run(_opts) do
+    version = Jarvis.MixProject.project() |> Keyword.fetch!(:version)
+    System.cmd("docker", ["build", "-t", "jarvis:" <> version, "-t", "jarvis:latest", "."], into: IO.stream(:stdio, :line))
+  end
+
+end
