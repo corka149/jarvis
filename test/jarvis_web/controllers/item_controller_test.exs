12278a10c67f850e39fe9379739ecaa97e16b3d5
corka149 <corka149@mailbox.org>
2019-10-27 16:16:05 +0100

    Implemented tests for item_controller

--- test/jarvis_web/controllers/item_controller_test.exs
+++ test/jarvis_web/controllers/item_controller_test.exs
@@ -44,4 +44,106 @@ defmodule JarvisWeb.ItemControllerTest do
     end
   end
 
+  describe "show" do
+    setup [:create_item]
+
+    test "show item", %{conn: conn, shopping_list: shopping_list, item: item, user: user} do
+      conn = init_test_session(conn, user_id: user.id)
+            |> get(Routes.item_path(conn, :show, shopping_list.id, item))
+
+      response_item = json_response(conn, 200)
+
+      assert %{"amount" => 2, "id" => _id, "name" => "apples"
+        } = response_item, "Responsed item is not in original item"
+    end
+  end
+
+  describe "create item" do
+    setup [:create_item]
+
+    test "use valid attrs for creating item", %{
+      conn: conn,
+      user: user,
+      shopping_list: shopping_list}
+    do
+      route = Routes.item_path(conn, :create, shopping_list.id)
+      conn = init_test_session(conn, user_id: user.id)
+              |>  post(route, item: @create_attrs)
+
+      create_item = json_response(conn, 201)
+
+      assert %{"amount" => 2, "id" => _id, "name" => "apples"} = create_item
+    end
+
+    test "use invalid attrs for creating item and expect error", %{
+      conn: conn,
+      user: user,
+      shopping_list: shopping_list}
+    do
+      route = Routes.item_path(conn, :create, shopping_list.id)
+      conn = init_test_session(conn, user_id: user.id)
+              |>  post(route, item: @invalid_attrs)
+
+      error_msg = json_response(conn, 422)
+
+      assert %{
+        "errors" => %{
+          "amount" => ["can't be blank"],
+          "name" => ["is invalid"]
+        }
+      } = error_msg
+    end
+  end
+
+  describe "update item" do
+    setup [:create_item]
+
+    test "update item with valid attrs", %{
+      conn: conn,
+      user: user,
+      shopping_list: shopping_list,
+      item: item
+    } do
+      conn = init_test_session(conn, user_id: user.id)
+              |> put(Routes.item_path(conn, :update, shopping_list.id, item),
+                  item: @update_attrs)
+
+      update_item = json_response(conn, 200)
+      assert %{"amount" => 5, "id" => _id, "name" => "cherries"} = update_item
+    end
+
+    test "update item with invalid attrs and expect error", %{
+      conn: conn,
+      user: user,
+      shopping_list: shopping_list,
+      item: item
+    } do
+      conn = init_test_session(conn, user_id: user.id)
+              |> put(Routes.item_path(conn, :update, shopping_list.id, item),
+                  item: @invalid_attrs)
+
+      update_item = json_response(conn, 422)
+      assert %{
+        "errors" => %{
+          "amount" => ["can't be blank"],
+          "name" => ["is invalid"]
+        }
+      } = update_item
+    end
+  end
+
+  describe "delete item" do
+    setup [:create_item]
+
+    test "delete existing item", %{
+      conn: conn,
+      user: user,
+      shopping_list: shopping_list,
+      item: item
+    } do
+      conn = init_test_session(conn, user_id: user.id)
+              |> delete(Routes.item_path(conn, :delete, shopping_list.id, item))
+      response(conn, 204)
+    end
+  end
 end
