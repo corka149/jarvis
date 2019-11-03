dc5fc46e828f1b0f8455efe081c51017fa7b8695
corka149 <corka149@mailbox.org>
2019-11-03 21:00:16 +0100

    Fixed user authorization

--- lib/jarvis/accounts/user_authorization.ex
+++ lib/jarvis/accounts/user_authorization.ex
@@ -3,6 +3,11 @@ defmodule Jarvis.Accounts.UserAuthorization do
   @behaviour JarvisWeb.AuthorizationBorder
 
   @impl JarvisWeb.AuthorizationBorder
+  def is_allowed_to_cross?(user, requested_user_id) when is_bitstring(requested_user_id) do
+    {requested_user_id, _} = Integer.parse(requested_user_id)
+    user.id == requested_user_id
+  end
+
   def is_allowed_to_cross?(user, requested_user_id) do
     user.id == requested_user_id
   end
