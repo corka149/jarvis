35eece191141e21540e4c1b69934e631adf00a22
corka149 <corka149@mailbox.org>
2020-04-01 22:21:15 +0200

    Extended shopping list views

--- lib/jarvis_web/views/shopping_list_view.ex
+++ lib/jarvis_web/views/shopping_list_view.ex
@@ -3,4 +3,12 @@ defmodule JarvisWeb.ShoppingListView do
   This view represents all possible response structures.
   """
   use JarvisWeb, :view
+
+  def edit_button(conn) do
+    link(
+      dgettext("shoppinglists", "Create list"),
+      to: Routes.shopping_list_path(conn, :new),
+      class: "btn primary-btn"
+    )
+  end
 end
