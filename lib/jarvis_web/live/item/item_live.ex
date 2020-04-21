e6f01b89eed01a7a3ea02e0ea07a1dffe4cdae3a
corka149 <corka149@mailbox.org>
2020-04-21 20:10:46 +0200

    Separated view and live controller for items

--- lib/jarvis_web/live/item/item_live.ex
+++
@@ -1,8 +0,0 @@
-defmodule JarvisWeb.ItemLive do
-  use Phoenix.LiveView
-
-  def mount(_params, _session, socket) do
-    temperature = 30
-    {:ok, assign(socket, :temperature, temperature)}
-  end
-end
---
+++ lib/jarvis_web/views/item_view.ex
@@ -0,0 +1,6 @@
+defmodule JarvisWeb.ItemView do
+  @moduledoc """
+  This view represents all "html.eex" and "html.leex" files in the directory "templates/item".
+  """
+  use JarvisWeb, :view
+end
