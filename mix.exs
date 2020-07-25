a1b52f5361386ea2c1f7b8cac443d6a18f3c92b7
corka149 <corka149@mailbox.org>
2020-07-25 12:57:54 +0200

    Add autocomplete to items form

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jarvis.MixProject do
   def project do
     [
       app: :jarvis,
-      version: "4.0.3",
+      version: "4.0.4",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
