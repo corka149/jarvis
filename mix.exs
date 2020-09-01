c8141a3bc061e6949dc528a27d0963230659a770
corka149 <corka149@mailbox.org>
2020-09-01 19:12:31 +0200

    Added changing colors of items on item list

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jarvis.MixProject do
   def project do
     [
       app: :jarvis,
-      version: "4.0.5",
+      version: "4.0.6",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
