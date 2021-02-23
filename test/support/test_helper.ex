9c0508403f770433e8f3bcb5dcd3ba6019bca6df
corka149 <corka149@mailbox.org>
2021-02-23 10:44:04 +0100

    Intro of shared gen_test_data for all tests

--- test/support/test_helper.ex
+++ test/support/test_helper.ex
@@ -10,4 +10,36 @@ defmodule Jarvis.TestHelper do
     email = Integer.to_string(:random.uniform(10_000_000)) <> user.email
     %{user | email: email}
   end
+
+  @doc """
+  Generate test data for other entites
+  """
+  def gen_test_data(data_type)
+
+  def gen_test_data(:user) do
+    valid_attrs_user = %{
+      email: "someemail@test.xyz",
+      name: "some name",
+      provider: "some provider",
+      token: "some token"
+    }
+
+    {:ok, user} =
+      update_with_unique_email(valid_attrs_user)
+      |> Jarvis.Accounts.create_user()
+
+    user
+  end
+
+  def gen_test_data(:user_group) do
+    valid_attrs_group = %{name: "some name"}
+    user = gen_test_data(:user)
+
+    {:ok, user_group} =
+      %{}
+      |> Enum.into(valid_attrs_group)
+      |> Jarvis.Accounts.create_user_group(user)
+
+    user_group
+  end
 end
