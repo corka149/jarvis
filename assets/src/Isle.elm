891ef4720c175da5fa2c5d62efba7e03452479fb
corka149 <corka149@mailbox.org>
2021-02-25 15:10:02 +0100

    Implemented header and group select for isle view

--- assets/src/Isle.elm
+++ assets/src/Isle.elm
@@ -1,8 +1,110 @@
 module Isle exposing (main)
 
-import Html
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.Events.Extra exposing (targetValueIntParse)
+import Json.Decode
 
 
-main : Html.Html msg
+
+-- MODEL
+
+
+type alias Model =
+    { groups : List Group
+    }
+
+
+type alias Group =
+    { id : Int
+    , name : String
+    }
+
+
+initialModel : Model
+initialModel =
+    { groups = getGroups
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = SelectedGroup Int
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( initialModel, Cmd.none )
+
+
+
+-- COMMANDS
+
+
+getGroups : List Group
+getGroups =
+    [ Group 0 "Admin"
+    , Group 1 "User"
+    , Group 2 "Guest"
+    ]
+
+
+
+-- VIEW
+
+
+view : Model -> Html.Html Msg
+view model =
+    div []
+        [ viewHeader
+        , viewUserGroups model
+        ]
+
+
+viewHeader : Html.Html msg
+viewHeader =
+    Html.div []
+        [ h1 [ class "center-align" ] [ text "Isles" ]
+        ]
+
+
+viewUserGroups : Model -> Html.Html Msg
+viewUserGroups model =
+    let
+        options =
+            List.map viewGroupOption model.groups
+    in
+    select [ on "change" (Json.Decode.map SelectedGroup targetValueIntParse) ]
+        options
+
+
+viewGroupOption : Group -> Html.Html Msg
+viewGroupOption group =
+    option
+        [ value (String.fromInt group.id)
+        ]
+        [ text group.name ]
+
+
+
+-- MAIN
+
+
+init : () -> ( Model, Cmd msg )
+init _ =
+    ( initialModel, Cmd.none )
+
+
+main : Program () Model Msg
 main =
-    Html.text "Isle!"
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
