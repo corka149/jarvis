3babe8a991b33d84adf7abb9326f7b0b7375ff1d
corka149 <corka149@mailbox.org>
2021-05-29 23:11:10 +0200

    Bumped elixir / otp version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Jarvis.MixProject do
     [
       app: :jarvis,
       version: "4.1.6",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
