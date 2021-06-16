66ddb3d919323aaf39be15e937c28cc8cc805e40
corka149 <corka149@mailbox.org>
2021-06-16 20:58:47 +0200

    Added manifest.json

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jarvis.MixProject do
   def project do
     [
       app: :jarvis,
-      version: "4.2.2",
+      version: "4.2.3",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
