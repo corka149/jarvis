81568bcff225d8e0a7f5d680117127f78bd4a13f
corka149 <corka149@mailbox.org>
2022-03-11 17:41:08 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jarvis.MixProject do
   def project do
     [
       app: :jarvis,
-      version: "4.2.7",
+      version: "4.2.8",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
