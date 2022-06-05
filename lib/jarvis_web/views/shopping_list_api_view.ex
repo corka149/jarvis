727c4c7f6ea25d112700bde7d37fdb4d0714b794
corka149 <corka149@mailbox.org>
2022-06-05 21:47:05 +0200

    Add group to list api response

--- lib/jarvis_web/views/shopping_list_api_view.ex
+++ lib/jarvis_web/views/shopping_list_api_view.ex
@@ -5,6 +5,7 @@ defmodule JarvisWeb.ShoppingListApiView do
   use JarvisWeb, :view
   alias JarvisWeb.ProductApiView
   alias JarvisWeb.ShoppingListApiView
+  alias JarvisWeb.UserGroupApiView
 
   def render("index.json", %{shopping_lists_api: shopping_lists}) do
     %{data: render_many(shopping_lists, ShoppingListApiView, "shopping_list.json")}
@@ -16,7 +17,13 @@ defmodule JarvisWeb.ShoppingListApiView do
 
   def render("shopping_list.json", %{shopping_list_api: shopping_list}) do
     products = render_many(shopping_list.products, ProductApiView, "product.json")
+    group = render_one(shopping_list.usergroup, UserGroupApiView, "user_group.json")
 
-    %{id: shopping_list.id, planned_for: shopping_list.planned_for, products: products}
+    %{
+      id: shopping_list.id,
+      planned_for: shopping_list.planned_for,
+      products: products,
+      belongs_to: group
+    }
   end
 end
