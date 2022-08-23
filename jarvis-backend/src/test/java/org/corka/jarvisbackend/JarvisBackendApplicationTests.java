9fb698d1895f640292ca0b5a5f0eba46b2c8d3e7
corka149 <corka149@mailbox.org>
2022-08-23 13:46:47 +0200

    add backend

---
+++ jarvis-backend/src/test/java/org/corka/jarvisbackend/JarvisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.corka.jarvisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarvisBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
