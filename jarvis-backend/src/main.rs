9c23d0e85db365a09e1f932f20aa197715ed9941
corka149 <corka149@mailbox.org>
2022-09-18 13:39:46 +0200

    Extract session store creation

--- jarvis-backend/src/main.rs
+++ jarvis-backend/src/main.rs
@@ -11,14 +11,18 @@ fn get_secret_key() -> Key {
     Key::generate()
 }
 
+fn new_session() -> SessionMiddleware<CookieSessionStore> {
+    let secret_key = get_secret_key();
+    let session_store = CookieSessionStore::default();
+    SessionMiddleware::new(session_store, secret_key)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let secret_key = get_secret_key();
-
     HttpServer::new(move || {
         App::new()
             .service(api_v1::api_v1())
-            .wrap(SessionMiddleware::new(CookieSessionStore::default(), secret_key.clone()))
+            .wrap(new_session())
     })
         .bind(("127.0.0.1", 8080))?
         .run()
