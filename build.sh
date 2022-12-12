53811838b0bf0f3647df4a33ed168b2efd53a3fd
corka149 <corka149@mailbox.org>
2022-12-12 21:45:38 +0100

    Update build process

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #/!/usr/bin/env zsh
 
-docker build -t jarvis:$(cat version) -t jarvis:latest . 
+docker build -t corka149/jarvis:$(cat version) -t corka149/jarvis:latest . 
+
+gum confirm "Push images?" && docker push corka149/jarvis:$(cat version) && docker push corka149/jarvis:latest
