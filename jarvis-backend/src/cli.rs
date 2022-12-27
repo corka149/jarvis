458228497d8bfef3ad8a9c82424286c73b3432ca
corka149 <corka149@mailbox.org>
2022-12-27 15:04:45 +0100

    Add basics for cli

---
+++ jarvis-backend/src/cli.rs
@@ -0,0 +1,15 @@
+/*!
+cli contains the following commands:
+
+* server
+* orga
+    * add
+    * delete
+    * show
+* user
+    * add
+    * delete
+    * passwd
+    * show
+
+ */
--- jarvis-backend/src/main.rs
+++ jarvis-backend/src/main.rs
@@ -7,6 +7,7 @@ use actix_web::{App, HttpServer};
 use env_logger::Env;
 
 mod api_v1;
+mod cli;
 mod configuration;
 mod dto;
 mod model;
