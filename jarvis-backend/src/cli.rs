f3751ec662a930bb71a8d3fbddd355c2ec275269
corka149 <corka149@mailbox.org>
2022-12-30 07:46:15 +0100

    Introducted server command

--- jarvis-backend/src/cli.rs
+++ jarvis-backend/src/cli.rs
@@ -13,3 +13,18 @@ cli contains the following commands:
     * show
 
  */
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+
+    #[command(subcommand)]
+    pub command: Commands
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Server{}
+}
--- jarvis-backend/src/main.rs
+++ jarvis-backend/src/main.rs
@@ -1,10 +1,8 @@
-use crate::configuration::Configuration;
-use crate::storage::MongoRepo;
-use actix_files as fs;
-use actix_web::middleware::Logger;
-use actix_web::web::Data;
-use actix_web::{App, HttpServer};
-use env_logger::Env;
+use actix_web::rt::Runtime;
+use clap::Parser;
+
+use crate::cli::{Cli, Commands};
+use crate::server::server;
 
 mod api_v1;
 mod cli;
@@ -13,25 +11,13 @@ mod dto;
 mod model;
 mod security;
 mod storage;
+mod server;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let config = Configuration::new().expect("No configuration found");
-    let mongo_repo = MongoRepo::new(config.database).await;
-    let static_file_dir = config.static_file_dir;
+fn main() -> std::io::Result<()> {
+    let cli: Cli = Cli::parse();
+    let rt = Runtime::new()?;
 
-    env_logger::init_from_env(Env::default().default_filter_or(config.logging.level));
-
-    HttpServer::new(move || {
-        App::new()
-            .app_data(Data::new(mongo_repo.clone()))
-            .service(api_v1::api_v1())
-            .service(fs::Files::new("/", static_file_dir.clone()).index_file("index.html"))
-            .wrap(security::new_session_store(&config.security))
-            .wrap(Logger::default())
-            .wrap(Logger::new("%a %{User-Agent}i"))
-    })
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+    match &cli.command {
+        Commands::Server{} => rt.block_on(server())
+    }
 }
