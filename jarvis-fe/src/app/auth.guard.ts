aefc7c8f97fbb9902ba60465670ac141d6f8cb99
corka149 <corka149@mailbox.org>
2023-01-04 21:52:01 +0100

    Logout when 401

--- jarvis-fe/src/app/auth.guard.ts
+++ jarvis-fe/src/app/auth.guard.ts
@@ -2,17 +2,25 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthenticationService) {}
+  private loginUrl: UrlTree;
+
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {
+    this.loginUrl = router.parseUrl('/login');
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -22,6 +30,11 @@ export class AuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    return this.authService.check();
+    return this.authService.check().pipe(
+      tap((success: boolean) => {
+        if (!success) this.authService.logOut();
+      }),
+      map((success: boolean) => (success ? success : this.loginUrl))
+    );
   }
 }
