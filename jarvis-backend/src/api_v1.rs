803f4af4b7c8d6d3f45a34a6189916b03c322fee
corka149 <corka149@mailbox.org>
2023-04-01 14:20:59 +0200

    Remove axum code

--- jarvis-backend/src/api_v1.rs
+++ jarvis-backend/src/api_v1.rs
@@ -1,7 +1,3 @@
-use actix_session::Session;
-use actix_web::body::{BoxBody, EitherBody};
-use actix_web::dev::{ServiceFactory, ServiceRequest, ServiceResponse};
-use actix_web::{delete, get, head, post, put, web, Error, HttpResponse, Responder, Scope};
 use std::collections::HashMap;
 
 use axum::extract::{Path, Query};
@@ -9,18 +5,12 @@ use axum::http::StatusCode;
 use axum::{routing, Json, Router};
 use axum_sessions::extractors::{ReadableSession, WritableSession};
 
-use serde::Deserialize;
-
 use crate::dto::List;
-use crate::{dto, service};
+use crate::service;
 
-use crate::security::{AuthTransformer, UserData};
+use crate::security::UserData;
 use crate::service::{JarvisError, LoginData};
 use crate::storage::MongoRepo;
-
-pub fn api_v1() -> Scope {
-    web::scope("/v1").service(auth_api()).service(list_api())
-}
 
 pub fn api_v1_a(repo: MongoRepo) -> Router {
     Router::new()
@@ -29,13 +19,6 @@ pub fn api_v1_a(repo: MongoRepo) -> Router {
 }
 
 // ===== AUTH =====
-
-fn auth_api() -> Scope {
-    web::scope("/auth")
-        .service(login)
-        .service(logout)
-        .service(check)
-}
 
 fn auth_api_a(repo: MongoRepo) -> Router {
     Router::new()
@@ -66,37 +49,11 @@ async fn login_a(
     StatusCode::OK
 }
 
-#[post("/login")]
-async fn login(
-    login_data: web::Json<LoginData>,
-    session: Session,
-    repo: web::Data<MongoRepo>,
-) -> HttpResponse {
-    let user_data = match service::login(login_data.0, &repo).await {
-        Ok(user_data) => user_data,
-        Err(err) => return into_response(err),
-    };
-
-    if let Err(err) = session.insert("user", user_data) {
-        log::error!("Could not add user data to session: {:?}", err);
-        return HttpResponse::InternalServerError().finish();
-    }
-
-    HttpResponse::Ok().finish()
-}
-
 /// POST "logout"
 async fn logout_a(mut session: WritableSession) -> StatusCode {
     session.remove("user");
 
     StatusCode::OK
-}
-
-#[post("/logout")]
-async fn logout(session: Session) -> impl Responder {
-    session.purge();
-
-    HttpResponse::Ok().finish()
 }
 
 /// HEAD "check"
@@ -107,43 +64,14 @@ async fn check_a(session: ReadableSession) -> StatusCode {
     }
 }
 
-#[head("/check")]
-async fn check(session: Session) -> impl Responder {
-    if let Ok(Some(_user)) = session.get::<UserData>("user") {
-        HttpResponse::Ok().finish()
-    } else {
-        HttpResponse::Unauthorized().finish()
-    }
-}
-
 // ===== LIST =====
-
-fn list_api() -> Scope<
-    impl ServiceFactory<
-        ServiceRequest,
-        Response = ServiceResponse<EitherBody<BoxBody>>,
-        Error = Error,
-        Config = (),
-        InitError = (),
-    >,
-> {
-    let auth_transformer = AuthTransformer {};
-
-    web::scope("/lists")
-        .wrap(auth_transformer)
-        .service(get_lists)
-        .service(create_list)
-        .service(get_list)
-        .service(delete_list)
-        .service(update_list)
-}
 
 fn list_api_a(repo: MongoRepo) -> Router {
     let lists_repo = repo.clone();
     let create_repo = repo.clone();
     let list_repo = repo.clone();
     let delete_repo = repo.clone();
-    let patch_repo = repo.clone();
+    let patch_repo = repo;
 
     Router::new()
         .route(
@@ -168,11 +96,6 @@ fn list_api_a(repo: MongoRepo) -> Router {
         )
 }
 
-#[derive(Debug, Deserialize)]
-struct GetListsQuery {
-    show_closed: Option<bool>,
-}
-
 /// GET ""
 async fn get_lists_a(
     repo: MongoRepo,
@@ -186,30 +109,10 @@ async fn get_lists_a(
         .map(|sc| sc == "true")
         .unwrap_or(false);
 
-    let lists = service::get_lists(&repo, show_closed, user_data)
+    service::get_lists(&repo, show_closed, user_data)
         .await
-        .map_err(into_response_a)?;
-
-    Ok(Json(lists))
-}
-
-#[get("")]
-async fn get_lists(
-    repo: web::Data<MongoRepo>,
-    query: web::Query<GetListsQuery>,
-    session: Session,
-) -> impl Responder {
-    let user_data = match get_user_data(session) {
-        Ok(user_data) => user_data,
-        Err(failed) => return failed,
-    };
-
-    let show_closed = query.show_closed.unwrap_or(false);
-
-    match service::get_lists(&repo, show_closed, user_data).await {
-        Ok(lists) => HttpResponse::Ok().json(lists),
-        Err(err) => into_response(err),
-    }
+        .map(Json)
+        .map_err(into_response_a)
 }
 
 /// POST ""
@@ -222,25 +125,8 @@ async fn create_list_a(
 
     service::create_list(list, &repo, user_data)
         .await
-        .map(|l| Json(l))
+        .map(Json)
         .map_err(into_response_a)
-}
-
-#[post("")]
-async fn create_list(
-    list: web::Json<List>,
-    repo: web::Data<MongoRepo>,
-    session: Session,
-) -> impl Responder {
-    let user_data = match get_user_data(session) {
-        Ok(user_data) => user_data,
-        Err(failed) => return failed,
-    };
-
-    match service::create_list(list.0, &repo, user_data).await {
-        Ok(list) => HttpResponse::Ok().json(list),
-        Err(err) => into_response(err),
-    }
 }
 
 /// GET "/:list_id"
@@ -253,25 +139,8 @@ async fn get_list_a(
 
     service::get_list(list_id, &repo, user_data)
         .await
-        .map(|l| Json(l))
+        .map(Json)
         .map_err(into_response_a)
-}
-
-#[get("/{list_id}")]
-async fn get_list(
-    list_id: web::Path<String>,
-    repo: web::Data<MongoRepo>,
-    session: Session,
-) -> impl Responder {
-    let user_data = match get_user_data(session) {
-        Ok(user_data) => user_data,
-        Err(failed) => return failed,
-    };
-
-    match service::get_list(list_id.into_inner(), &repo, user_data).await {
-        Ok(list) => HttpResponse::Ok().json(list),
-        Err(err) => into_response(err),
-    }
 }
 
 /// DELETE "/{list_id}"
@@ -286,23 +155,6 @@ async fn delete_list_a(
         .await
         .map(|_| StatusCode::NO_CONTENT)
         .map_err(into_response_a)
-}
-
-#[delete("/{list_id}")]
-async fn delete_list(
-    list_id: web::Path<String>,
-    repo: web::Data<MongoRepo>,
-    session: Session,
-) -> impl Responder {
-    let user_data = match get_user_data(session) {
-        Ok(user_data) => user_data,
-        Err(failed) => return failed,
-    };
-
-    match service::delete_list(list_id.into_inner(), &repo, user_data).await {
-        Ok(_) => HttpResponse::NoContent().finish(),
-        Err(err) => into_response(err),
-    }
 }
 
 /// PUT "/{list_id}"
@@ -320,53 +172,12 @@ async fn update_list_a(
         .map_err(into_response_a)
 }
 
-#[put("/{list_id}")]
-async fn update_list(
-    list_id: web::Path<String>,
-    list: web::Json<dto::List>,
-    repo: web::Data<MongoRepo>,
-    session: Session,
-) -> impl Responder {
-    let user_data = match get_user_data(session) {
-        Ok(user_data) => user_data,
-        Err(failed) => return failed,
-    };
-
-    match service::update_list(list_id.into_inner(), list.0, &repo, user_data).await {
-        Ok(_) => HttpResponse::NoContent().finish(),
-        Err(err) => into_response(err),
-    }
-}
-
 // ===== ===== HELPER ===== =====
-
-fn get_user_data(session: Session) -> Result<UserData, HttpResponse> {
-    match session.get::<UserData>("user") {
-        Ok(Some(user_data)) => {
-            log::debug!("Received data: {:?}", user_data);
-            Ok(user_data)
-        }
-        Ok(None) => Err(HttpResponse::Unauthorized().finish()),
-        Err(err) => {
-            log::error!("Error while getting user data: {:?}", err);
-            Err(HttpResponse::InternalServerError().finish())
-        }
-    }
-}
 
 fn get_user_data_a(session: ReadableSession) -> Result<UserData, StatusCode> {
     match session.get::<UserData>("user") {
         Some(user_data) => Ok(user_data),
         None => Err(StatusCode::UNAUTHORIZED),
-    }
-}
-
-fn into_response(jarvis_err: service::JarvisError) -> HttpResponse {
-    match jarvis_err {
-        JarvisError::InvalidData(msg) => HttpResponse::BadRequest().body(msg),
-        JarvisError::NotFound => HttpResponse::NotFound().finish(),
-        JarvisError::Unauthorized => HttpResponse::Unauthorized().finish(),
-        JarvisError::ServerFailed => HttpResponse::InternalServerError().finish(),
     }
 }
 
