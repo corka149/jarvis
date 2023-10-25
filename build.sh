90ffe7c351967593e196e919cea388d8c9ac1241
corka149 <corka149@mailbox.org>
2023-10-25 22:03:09 +0200

    Init

--- build.sh
+++
@@ -1,5 +0,0 @@
-#/!/usr/bin/env zsh
-
-docker build -t corka149/jarvis:$(cat version) -t corka149/jarvis:latest . 
-
-gum confirm "Push images?" && docker push corka149/jarvis:$(cat version) && docker push corka149/jarvis:latest
