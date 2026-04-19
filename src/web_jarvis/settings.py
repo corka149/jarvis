ec40f8f21a7382da0507a80c2718ec893f4aeb2f
corka149 <corka149@mailbox.org>
2026-04-19 17:27:21 +0200

    Configure redis with password

--- src/web_jarvis/settings.py
+++ src/web_jarvis/settings.py
@@ -149,7 +149,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.getenv("REDIS_HOST") or "redis://127.0.0.1:6379"],
+            "hosts": [os.getenv("REDIS_HOST") or "redis://:a_password@127.0.0.1:6379"],
         },
     },
 }
